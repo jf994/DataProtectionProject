33542a6a74c1022cd97bf5018bf4009332156e91
jf994 <jacopo.favaro94@gmail.com>
2019-03-10 11:35:34 +0100

    not working matrix product

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ support = file_mat_ds['support']
 file_mat_dist = io.loadmat('distorted_5000.mat')
 distorted = file_mat_dist['distorted']
 p = file_mat_dist['p']
+num_clients = len(distorted[:, 1])
 
 # calcolo R1(p), R0(p), R per calcolare poi la privacy ottenuta
 
@@ -27,4 +28,29 @@ privacy = (1 - R) * 100
 
 print("User Privacy raggiunta: {}%".format(privacy))
 
-# Effettuiamo il mining sul ds distorto
+# Stima singleton supports colonna 1
+
+j = 1
+
+# conto gli uni
+C1_D = 0
+for i in range(0, (num_clients - 1)):
+    if distorted[i,j] == 1:
+        C1_D += 1
+
+# conto gli zeri
+C0_D = num_clients - C1_D
+
+# DA METTERE APPOSTO
+M = [[0 for x in range(2)]for y in range(2)]
+M[0][0] = p
+M[0][1] = 1-p
+M[1][0] = 1-p
+M[1][1] = p
+C_D = [[0 for x in range(2)]for y in range(1)]
+C_D[0][0] = C1_D
+C_D[0][1] = C0_D
+
+C_T = np.linalg.inv(M) @ C_D
+print(C_T)
+
