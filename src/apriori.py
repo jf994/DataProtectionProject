d4adb21e5ed6d6b524041cae81cd53123b3712de
jf994 <jacopo.favaro94@gmail.com>
2019-03-12 11:39:40 +0100

    apriori function working

--- src/apriori.py
+++ src/apriori.py
@@ -5,15 +5,22 @@ from apyori import apriori
 from scipy import io
 from scipy import sparse
 
-file_mat_ds = io.loadmat('dataset_500.mat')
+file_mat_ds = io.loadmat('../docs/dataset.mat')
 dataset = sparse.csr_matrix(file_mat_ds['dataset'])
+items = file_mat_ds['items']
 
-records = []
-for i in range(0, 500):
-    records.append([str(dataset.A[i,j]) for j in range(0, 200)])
+print("Initialization...\n")
+records = [["nan" for x in range(120)] for y in range(7500)]
+temp = []
+tot = sparse.find(dataset)
 
-print('ciao\n')
+for i in range(0, len(tot[0])):
+    print(str(i)+"\n")
+    records[tot[0][i]][tot[1][i]] = str(items[tot[1][i]])
+
+print(records)
+print("Doing association...\n")
 association_rules = apriori(records, min_support=0.0045, min_confidence=0.2, min_lift=3, min_length=2)
 association_results = list(association_rules)
 
-print(association_rules[0])
+print(association_results[0])
---
+++ src/modify_dataset.m
@@ -0,0 +1,41 @@
+clear
+clc
+
+[num, text, raw] = xlsread('../docs/store_data.xlsx');
+text = text(2:end-1,:);
+clear raw num
+
+% prendo tutti gli elementi singoli presenti all'interno del dataset
+items = text(1,:);
+
+for i=2:size(text,1)
+    for j=1:size(text,2)
+        k = 1;
+        flag = false;
+        while k < length(items) + 1
+            if strcmp(items(k), text(i,j)) || strcmp("", text(i,j))
+                flag = true;
+                break;
+            end
+            k = k + 1;
+        end
+        
+        if ~flag
+            items = [items text(i,j)];
+        end
+    end
+end
+
+% creo il dataset binario
+items = sort(items);
+dataset = sparse(size(text,1), length(items));
+
+for i=1:size(text,1)
+    dataset(i,:) = ismember(items, text(i,:));
+end
+
+clear flag i j k
+
+items = char(items);
+text = char(text);
+save('../docs/dataset.mat');
